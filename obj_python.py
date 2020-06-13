244e949fb263606837125dd33f11a635e88a839b
amaca <amaca@umich.edu>
2020-06-13 13:41:04 -0400

    jahn's first pull

--- obj_python.py
+++ obj_python.py
@@ -9,6 +9,7 @@ interpreter.allocate_tensors()
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+print(output_details)
 
 # Test model on random input data.
 input_shape = input_details[0]['shape']
