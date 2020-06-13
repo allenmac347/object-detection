22ecf54e22d86196dc74af1e55dd8f452891292d
amaca <amaca@umich.edu>
2020-06-13 14:35:32 -0400

    first pull

--- obj_python.py
+++ obj_python.py
@@ -2,14 +2,21 @@ import tflite_runtime.interpreter as tflite
 import numpy as np
 import os
 
+"""
+Index 0: Locations
+Index 1: Classes
+Index 2: Scores
+Index 3: Number and detections 
+"""
+
+
 # Load TFLite model and allocate tensors.
-interpreter = tflite.Interpreter(model_path="models/mobilenet-quant/mobilenet_v1_1.0_160_quant.tflite")
+interpreter = tflite.Interpreter(model_path='models/ssd_mobilenet/detect.tflite')
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-print(output_details)
 
 # Test model on random input data.
 input_shape = input_details[0]['shape']
@@ -21,4 +28,5 @@ interpreter.invoke()
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
 output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data[0])
 print(output_data)
